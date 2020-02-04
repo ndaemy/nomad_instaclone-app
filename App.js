1a0f02c3a595b16bc95a295db91c03cfe65526d2
yuyaebean <yuyaebean@gmail.com>
2020-02-05 04:18:44 +0900

    10.2 Preload cache

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage } from 'react-native';
+
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Asset } from 'expo-asset';
 import { Ionicons } from '@expo/vector-icons';
 
+import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import { ApolloProvider } from 'react-apollo-hooks';
+
+import apolloClientOptions from './apollo';
+
 export default () => {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font
       });
       await Asset.loadAsync([require('./assets/logo.png')]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions
+      });
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -21,10 +40,12 @@ export default () => {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded ? (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
